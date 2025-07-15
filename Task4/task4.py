378e79324a56bb69b5b0cfc13b8f04aeea73b02f
shekhar-11 <rajshekhar111201@gmail.com>
2025-07-15 12:32:23 +0530

    created a backup dir with files getting stored as zip file

--- Task4/task4.py
+++ Task4/task4.py
@@ -1,3 +1,27 @@
-import shutil
 import os
 
+
+os.chdir('/home/rajs11')
+try:
+    os.system('rm -r Backup')
+except Exception as e:
+    print(e)
+
+
+try:
+    os.mkdir('/home/rajs11/Backup')
+    print("Dir created")
+except Exception as e:
+    print(e)
+
+try:
+    os.mkdir('/home/rajs11/zipDir')
+    os.mkdir('/home/rajs11/zipDir/zipFinal')
+except Exception as e: 
+    print(e)    
+os.system('cp -r /home/rajs11/Books /home/rajs11/zipDir/zipFinal')
+
+os.chdir('/home/rajs11/zipDir')
+os.system('zip -r zipFinal.zip zipFinal')
+# os.system('rm -r zipFinal')
+os.system('cp zipFinal.zip /home/rajs11/Backup')
